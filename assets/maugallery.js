3bb7664b1643af45f7667f6b4af87cd8772f7439
elias06110 <elias06110@gmail.com>
2023-12-02 22:33:36 +0100

    version finale

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -154,11 +154,17 @@
         }
       });
       next =
-        imagesCollection[index] ||
+        imagesCollection[index-1] ||
         imagesCollection[imagesCollection.length - 1];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
+
+
+
+
+
     nextImage() {
+      debugger
       let activeImage = null;
       $("img.gallery-item").each(function() {
         if ($(this).attr("src") === $(".lightboxImage").attr("src")) {
@@ -192,7 +198,7 @@
           index = i;
         }
       });
-      next = imagesCollection[index] || imagesCollection[0];
+      next = imagesCollection[index+1] || imagesCollection[0];
       $(".lightboxImage").attr("src", $(next).attr("src"));
     },
     createLightBox(gallery, lightboxId, navigation) {
